9d58e103ee89cb1334fc8fe53c316d9b73ab8fd3
Audrey Carlsen <acarlsen@seattletimes.com>
2016-06-21 14:48:17 -0700

    icons are clickable

--- src/js/main.js
+++ src/js/main.js
@@ -6,20 +6,20 @@ require("component-responsive-frame/child");
 var animate= require("./lib/animateScroll");
 var qsa = require("./lib/qsa");
 var items = qsa(".item");
-qsa(".icon").forEach(function(i) {
+// qsa(".icon").forEach(function(i) {
 
-  i.addEventListener("click", function(e) {
-    console.log("hi");
-    var order = i.getAttribute("data-order");
-    items.forEach(function(item) {
-      if (item.getAttribute("data-order") == order) {
-        animateScroll(item);
-        console.log(item);
-      }
-    });
+//   i.addEventListener("click", function(e) {
+//     console.log("hi");
+//     var order = i.getAttribute("data-order");
+//     items.forEach(function(item) {
+//       if (item.getAttribute("data-order") == order) {
+//         animateScroll(item);
+//         console.log(item);
+//       }
+//     });
 
-  });
-});
+//   });
+// });
 
 
 
@@ -28,4 +28,6 @@ document.querySelector(".map").addEventListener("click", function(e) {
   console.log(e.target);
 });
 
-console.log(document.querySelector(".map"));
+// window.body.addEventListener("click", function(e) {
+//   console.log(e.target)
+// })
