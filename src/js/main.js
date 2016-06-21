52701cac8decffe4ac8426084fd483fe47250815
Elif Koc <elifkoc@nyu.edu>
2016-06-21 14:33:20 -0700

    Adding event listeners

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,29 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+var animate= require("./lib/animateScroll");
+var qsa = require("./lib/qsa");
+var items = qsa(".item");
+qsa(".icon").forEach(function(i) {
+
+  i.addEventListener("click", function(e) {
+    console.log("hi");
+    var order = i.getAttribute("data-order");
+    items.forEach(function(item) {
+      if (item.getAttribute("data-order") == order) {
+        animateScroll(item);
+        console.log(item);
+      }
+    });
+
+  });
+});
+
+
+
+
+document.querySelector(".map").addEventListener("click", function(e) {
+  console.log(e.target);
+});
+
+console.log(document.querySelector(".map"));
