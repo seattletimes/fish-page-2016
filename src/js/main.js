5ada613028927be53afb3e3db145627fae78b691
Elif Koc <elifkoc@nyu.edu>
2016-06-22 12:35:25 -0700

    Added scroll animations

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,35 @@ require("component-responsive-frame/child");
 var animate = require("./lib/animateScroll");
 var qsa = require("./lib/qsa");
 var items = qsa(".item");
-qsa(".icon").forEach(function(i) {
-  i.addEventListener("click", function(e) {
-    var order = i.getElementsByClassName("order")[0];
+var debounce = require("./lib/debounce");
 
-    //This is the icon's order number.
-    var pos = order.innerHTML;
+var mapContainer = document.querySelector(".map");
+var header = document.querySelector(".hed");
+var legend = document.querySelector(".legend");
 
-    items.forEach(function(item) {
-      if (item.dataset.order == pos) {
-        console.log(item);
-        animate(item);
-      }
-    });
+window.addEventListener("scroll", debounce(function(e) {
+  var bounds = legend.getBoundingClientRect();
+  if (bounds.bottom < 0) {
+    mapContainer.classList.add("after-scroll");
+  } else {
+    mapContainer.classList.remove("after-scroll");
+  }
+}));
 
+var onClickIcon = function(e) {
+
+  //This is the icon's order number.
+  var pos = this.getAttribute("data-index");
+
+  console.log(pos);
+
+  items.forEach(function(item) {
+    if (item.dataset.order == pos) {
+      console.log(item);
+      animate(item);
+    }
   });
-});
+
+}
+
+qsa(".icon").forEach(i => i.addEventListener("click", onClickIcon));
